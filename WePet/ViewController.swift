75b4833bcb7b55cdd289a5b5efa92f43a2f00d43
양혜리 <yangyang6424@gmail.com>
2019-10-12 15:41:23 +0900

    Create Project

---
+++ WePet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WePet
+//
+//  Created by 양혜리 on 12/10/2019.
+//  Copyright © 2019 Depromeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
