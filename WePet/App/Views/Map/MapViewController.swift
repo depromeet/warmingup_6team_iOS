804de2e34cc08e2c5c78da1dbeab781d615e90f5
양혜리 <yangyang6424@gmail.com>
2019-10-30 20:16:52 +0900

    [Feature] 구글 맵 object 생성

--- WePet/App/Views/Map/MapViewController.swift
+++ WePet/App/Views/Map/MapViewController.swift
@@ -7,17 +7,86 @@
 //
 
 import UIKit
+import GoogleMaps
+import SnapKit
 
 protocol MapViewControllerType: AnyObject {
     func reload()
 }
 
 class MapViewController: BaseViewController {
-
+    
     var presenter: MapPresenterType?
+    
+    // MARK: MapView
+    private lazy var mapView: GMSMapView = {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView(frame: .zero, camera: camera)
+        mapView.mapType = .normal
+        mapView.setMinZoom(5.0, maxZoom: 20.0)
+        mapView.settings.zoomGestures = true
+        mapView.settings.setAllGesturesEnabled(true)
+        mapView.settings.compassButton = true
 
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        mapView.delegate = self
+        view.addSubview(mapView)
+        return mapView
+    }()
+    
+    
+    private lazy var bottomSheet: UIView = {
+        let bottomSheet: UIView = UIView()
+        bottomSheet.backgroundColor = .green
+        view.addSubview(bottomSheet)
+        return bottomSheet
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        configurationConstarint()
+        addBottomSheetView()
+    }
+    
+    func configurationConstarint() {
+        mapView.snp.makeConstraints {
+            $0.centerX.centerY.width.height.equalToSuperview()
+        }
+        
+        bottomSheet.snp.makeConstraints {
+            $0.leading.trailing.bottom.width.equalToSuperview()
+            $0.height.equalTo(100.0)
+        }
+    }
+    
+    func addBottomSheetView() {
+        // 1- Init bottomSheetVC
+        let bottomSheetVC = BottomSheetViewController()
+
+        // 2- Add bottomSheetVC as a child view
+        addChild(bottomSheetVC)
+        view.addSubview(bottomSheetVC.view)
+        bottomSheetVC.didMove(toParent: self)
+
+        // 3- Adjust bottomSheet frame and initial position.
+        let height = view.frame.height
+        let width  = view.frame.width
+        bottomSheetVC.view.frame = CGRect(x: 0, y: self.view.frame.maxY, width: width, height: height)
+    }
+}
+
+extension MapViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+    }
+    
+    func mapViewDidFinishTileRendering(_ mapView: GMSMapView) {
     }
 }
 
