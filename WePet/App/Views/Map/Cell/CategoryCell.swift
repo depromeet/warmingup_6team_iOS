6bef4ddfcc0d666aff2c0fe91366f1eba0f9ee46
양혜리 <yangyang6424@gmail.com>
2019-11-04 21:39:33 +0900

    [Feature] 지도 줌 조절 버튼 추가

---
+++ WePet/App/Views/Map/Cell/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  WePet
+//
+//  Created by 양혜리 on 04/11/2019.
+//  Copyright © 2019 depromeet. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
--- WePet/App/Views/Map/MapViewController.swift
+++ WePet/App/Views/Map/MapViewController.swift
@@ -27,9 +27,39 @@ class MapViewController: BaseViewController {
         return button
     }()
     
+    private lazy var subtractionButton: UIButton = {
+        let button: UIButton = UIButton(type: .custom)
+        button.setImage(UIImage(named: "subtraction"), for: .normal)
+        button.setImage(UIImage(named: "subtraction"), for: .normal)
+        button.addTarget(self, action: #selector(pressedSubtractionButton), for: .touchUpInside)
+        button.backgroundColor = .white
+        view.addSubview(button)
+        return button
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let button: UIButton = UIButton(type: .custom)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.addTarget(self, action: #selector(pressedPlusButtonButton), for: .touchUpInside)
+        button.backgroundColor = .white
+        view.addSubview(button)
+        return button
+    }()
+    
+    private lazy var compassButton: UIButton = {
+        let button: UIButton = UIButton(type: .custom)
+        button.setImage(UIImage(named: "compass"), for: .normal)
+        button.setImage(UIImage(named: "compass"), for: .normal)
+        button.addTarget(self, action: #selector(pressedCompassButtonButton), for: .touchUpInside)
+        button.backgroundColor = .white
+        view.addSubview(button)
+        return button
+    }()
+    
     // MARK: MapView
     private lazy var mapView: GMSMapView = {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.5468225, longitude: 126.9478771, zoom: 15.0)
         let mapView = GMSMapView(frame: .zero, camera: camera)
         mapView.mapType = .normal
         mapView.setMinZoom(5.0, maxZoom: 20.0)
@@ -38,10 +68,19 @@ class MapViewController: BaseViewController {
         mapView.settings.compassButton = true
 
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 37.5468225, longitude: 126.9478771)
+        marker.title = "서울창업허브"
+        marker.icon = UIImage(named: "marker")
         marker.map = mapView
+        
+        let marker1 = GMSMarker()
+        marker1.position = CLLocationCoordinate2D(latitude: 37.54322, longitude: 126.9493873)
+        marker1.title = "공덕역"
+        marker1.icon = UIImage(named: "marker")
+        marker1.map = mapView
+    
+        
+    
         mapView.delegate = self
         view.addSubview(mapView)
         return mapView
@@ -52,15 +91,38 @@ class MapViewController: BaseViewController {
         view.backgroundColor = .green
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
         configurationConstarint()
-//        addBottomSheetView()
-        addBottomInfoView()
+        addBottomSheetView()
+//        addBottomInfoView()
     }
     
     func configurationConstarint() {
+        mapView.snp.makeConstraints {
+            $0.centerX.centerY.width.height.equalToSuperview()
+        }
+        
         backButton.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(26.0)
             $0.top.equalToSuperview().offset(50.0)
         }
+        
+        subtractionButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.snp.centerY)
+            $0.trailing.equalToSuperview().offset(-21.0)
+            $0.width.height.equalTo(35.0)
+        }
+        
+        plusButton.snp.makeConstraints {
+            $0.bottom.equalTo(subtractionButton.snp.top)
+            $0.trailing.equalToSuperview().offset(-21.0)
+            $0.width.height.equalTo(35.0)
+        }
+        
+        compassButton.snp.makeConstraints {
+            $0.top.equalTo(subtractionButton.snp.bottom).offset(8.0)
+            $0.trailing.equalToSuperview().offset(-21.0)
+            $0.width.height.equalTo(35.0)
+        }
+
     }
     
     func addBottomInfoView() {
@@ -124,13 +186,41 @@ class MapViewController: BaseViewController {
     @objc func pressedBackButton() {
         navigationController?.popViewController(animated: true)
     }
+    
+    @objc func pressedSubtractionButton() {
+        let currentZoom = mapView.camera.zoom
+        let camera = GMSCameraPosition(latitude: mapView.camera.target.latitude, longitude: mapView.camera.target.longitude, zoom: currentZoom - 0.1)
+        mapView.camera = camera
+    }
+    
+    @objc func pressedPlusButtonButton() {
+        let currentZoom = mapView.camera.zoom
+        let camera = GMSCameraPosition(latitude: mapView.camera.target.latitude, longitude: mapView.camera.target.longitude, zoom: currentZoom + 0.1)
+        mapView.camera = camera
+    }
+    
+    @objc func pressedCompassButtonButton() {
+        navigationController?.popViewController(animated: true)
+    }
 }
 
 extension MapViewController: GMSMapViewDelegate {
     func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        
+    }
+    
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+        // Move
+        
     }
     
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        marker.icon = UIImage(named: "active_marker")
+        return true
     }
     
     func mapViewDidFinishTileRendering(_ mapView: GMSMapView) {
